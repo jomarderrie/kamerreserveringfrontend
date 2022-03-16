7467318d44b091642e6a0592007a1c5c9a4257ff
nathanganesh <deeznutsgottem6969@gmail.com>
2022-03-16 07:49:24 +0100

    pagination adding base features

--- src/context/KamersContext.js
+++ src/context/KamersContext.js
@@ -1,75 +1,79 @@
-import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { toast } from "react-toastify";
+import React, {useContext, useState} from "react";
+import {useHistory} from "react-router-dom";
+import {toast} from "react-toastify";
 import {deleteKamer, getPaginatedKamers} from "../functions/kamers";
+
 export const KamersContext = React.createContext();
 
-export default function KamerProvider({ children }) {
-  const [kamers, setKamers] = useState([]);
+export default function KamerProvider({children}) {
+    const [kamers, setKamers] = useState([]);
 
-  const[pageKamerInfo, setPageKamerInfo] = useState({
-    pageNo: 0,
-    pageSize: 5,
-    sortBy:"naam",
-    totalPages: 0,
-    totalElements: 0,
-  })
-
-  let history = useHistory();
-
-  const deleteKamerOnClick = async (naam) => {
-    // console.log(e.target);
-    console.log(naam, "oekoek");
-    await deleteKamer(naam)
-      .then((res, err) => {
-        if (err) {
-          toast.error("Error met het toevoegen van een kamer");
-        } else {
-
-          setKamers((prevKamer) => {
-            return prevKamer.filter((kamer) => {
-              return kamer.naam !== naam;
-            });
-          });
-          history.push("/kamers")
-
-          toast.success(`Delete kamer met naam ${naam}`);
-        }
-      })
-      .catch((k) => {
-        if (!k.response?.data?.message) {
-          toast.error(k);
-        } else {
-          toast.error(k.response.data.message);
-        }
-
-        return Promise.reject(k);
-      });
-  };
-
-  const getPaginatedKamersContext = async () =>{
-    console.log(pageKamerInfo.pageSize, "page")
-  await  getPaginatedKamers(pageKamerInfo.pageNo,pageKamerInfo.pageSize ).then((res, err) => {
-      setKamers(res.data.content);
-      setPageKamerInfo(
-          {totalPages:res.data.totalPages,
-            totalElements:res.data.totalElements,
-            pageNo:res.data.currentPage,
-            pageSize:res.data.kamersPerPage}
-      )
-    });
-
-  }
-
-  const filterRooms = (searchKeyWord) =>{
-    kamers.filter((item) =>{
-
+    const [pageKamerInfo, setPageKamerInfo] = useState({
+        pageNo: 0,
+        pageSize: 5,
+        sortBy: "naam",
+        totalPages: 0,
+        totalElements: 0,
     })
-  }
 
-  return (
-    <KamersContext.Provider value={{ kamers, setKamers, deleteKamerOnClick, pageKamerInfo, setPageKamerInfo,getPaginatedKamersContext    }}>
-      {children}
-    </KamersContext.Provider>
-  );
+    let history = useHistory();
+
+    const deleteKamerOnClick = async (naam) => {
+        // console.log(e.target);
+        console.log(naam, "oekoek");
+        await deleteKamer(naam)
+            .then((res, err) => {
+                if (err) {
+                    toast.error("Error met het toevoegen van een kamer");
+                } else {
+
+                    setKamers((prevKamer) => {
+                        return prevKamer.filter((kamer) => {
+                            return kamer.naam !== naam;
+                        });
+                    });
+                    history.push("/kamers")
+
+                    toast.success(`Delete kamer met naam ${naam}`);
+                }
+            })
+            .catch((k) => {
+                if (!k.response?.data?.message) {
+                    toast.error(k);
+                } else {
+                    toast.error(k.response.data.message);
+                }
+
+                return Promise.reject(k);
+            });
+    };
+
+    const getPaginatedKamersContext = async (currentPage, pageSize, sortBy = "naam") => {
+        console.log(pageKamerInfo, "asd")
+        await getPaginatedKamers(currentPage, pageSize, sortBy).then((res, err) => {
+            setKamers(res.data.content);
+            setPageKamerInfo(
+                {
+                    totalPages: res.data.totalPages,
+                    totalElements: res.data.totalElements,
+                    pageNo: res.data.currentPage,
+                    pageSize: res.data.kamersPerPage
+                }
+            )
+        });
+
+    }
+
+    const filterRooms = (searchKeyWord) => {
+        kamers.filter((item) => {
+
+        })
+    }
+
+    return (
+        <KamersContext.Provider
+            value={{kamers, setKamers, deleteKamerOnClick, pageKamerInfo, setPageKamerInfo, getPaginatedKamersContext}}>
+            {children}
+        </KamersContext.Provider>
+    );
 }
--- src/pages/kamer/Kamers.js
+++ src/pages/kamer/Kamers.js
@@ -1,80 +1,101 @@
-import React, { Component, useContext, useEffect, useState } from "react";
+import React, {Component, useContext, useEffect, useState} from "react";
 import {
-  deleteKamerMetFetch,
-  getPaginatedKamers,
-  getImageFromDb,
+    deleteKamerMetFetch,
+    getPaginatedKamers,
+    getImageFromDb,
 } from "../../functions/kamers";
-import { set } from "react-hook-form";
-import { FlexBox, setUpDownPadding } from "../../styled/styles";
+import {set} from "react-hook-form";
+import {FlexBox, setUpDownPadding} from "../../styled/styles";
 import KamerCard from "../../components/kamer/KamerCard";
-import { Link, useHistory } from "react-router-dom";
-import { toast } from "react-toastify";
-import { deleteKamer } from "../../functions/kamers";
-import { Image } from "../../styled/Image";
+import {Link, useHistory} from "react-router-dom";
+import {toast} from "react-toastify";
+import {deleteKamer} from "../../functions/kamers";
+import {Image} from "../../styled/Image";
 import Input from "../../styled/globals/Input";
 import InputWithImage from "../../styled/globals/Input";
-import { FlexBoxUpDown, GridImages } from "./../../styled/globals/StyledFlexBoxContainer";
-import { ButtonLink, StyledButtonLink, StyledRouterLink } from "./../../styled/globals/StyledRouterLink";
+import {FlexBoxUpDown, GridImages} from "./../../styled/globals/StyledFlexBoxContainer";
+import {ButtonLink, StyledButtonLink, StyledRouterLink} from "./../../styled/globals/StyledRouterLink";
 import SideBar from "../../components/navbar/SideBar";
 import {deleteKamerOnClick} from "../../helpers/kamerDelete"
-import { KamersContext } from "../../context/KamersContext";
+import {KamersContext} from "../../context/KamersContext";
 import * as url from "url";
+
 function Kamers(props) {
-  const {kamers, setKamers, pageKamerInfo, setPageKamerInfo, getPaginatedKamersContext}= useContext(KamersContext);
-  const [loading, setLoading] = useState(false);
+    const {kamers, setKamers, pageKamerInfo, setPageKamerInfo, getPaginatedKamersContext} = useContext(KamersContext);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() =>{
-    const urlSearchParams = new URLSearchParams(props.location.search)
-    console.log(urlSearchParams.get("pageNo"), "ke")
-    urlSearchParams.get("pageSize")
-    for (const [key, value] of urlSearchParams.entries()) {
-      console.log(key, value, "ts1")
+    useEffect(() => {
+    }, [props.location.search])
+
+    const getPageParams = () =>{
+        const urlSearchParams = new URLSearchParams(props.location.search)
+        if (urlSearchParams.get("sortBy")) {
+
+            setPageKamerInfo({pageNo:urlSearchParams.get("pageNo"), pageSize:urlSearchParams.get("pageSize"), sortBy: urlSearchParams.get("sortBy")})
+            return "a"
+        } else {
+          setPageKamerInfo(prev =>{
+
+              ...prev,
+                  {
+              {pageNo:urlSearchParams.get("pageNo"), pageSize:urlSearchParams.get("pageSize")}
+                  }
+          })
+            return "a"
+        }
     }
-  },[props.location.search])
 
-  useEffect(() => {
-    setLoading(true);
-    getPaginatedKamersContext().then(() =>{
-      setLoading(false)
-    })
+    useEffect(() => {
+        setLoading(true);
+        console.log(pageKamerInfo.pageNo, pageKamerInfo.pageSize, pageKamerInfo.sortBy, "t123")
+        if (props.location.search){
+            getPageParams()
 
-  }, []);
+                getPaginatedKamersContext(pageKamerInfo.pageNo, pageKamerInfo.pageSize, pageKamerInfo.sortBy).then(() => {
+                    setLoading(false)
+                })
 
-  const deleteKamerOnClick = async (naam, setKamers) => {
-    // console.log(e.target);
-  await deleteKamer(naam)
-    .then((res, err) => {
-      if (err) {
-        toast.error("Error met het toevoegen van een kamer");
-      } else {
-        setKamers((prevKamer) => {
-          return prevKamer.filter((kamer) => {
-            return kamer.naam !== naam;
-          });
-        });
-        toast.success(`Delete kamer met naam ${naam}`);
-      }
-    })
-    .catch((k) => {
-      toast.error(k.response.data.message);
+        }else{
+            getPaginatedKamersContext(pageKamerInfo.pageNo, pageKamerInfo.pageSize, pageKamerInfo.sortBy).then(() => {
+                setLoading(false)
+            })
+        }
 
-      return Promise.reject(k);
-    });
-};
+    }, []);
+
+    const deleteKamerOnClick = async (naam, setKamers) => {
+        // console.log(e.target);
+        await deleteKamer(naam)
+            .then((res, err) => {
+                if (err) {
+                    toast.error("Error met het toevoegen van een kamer");
+                } else {
+                    setKamers((prevKamer) => {
+                        return prevKamer.filter((kamer) => {
+                            return kamer.naam !== naam;
+                        });
+                    });
+                    toast.success(`Delete kamer met naam ${naam}`);
+                }
+            })
+            .catch((k) => {
+                toast.error(k.response.data.message);
+
+                return Promise.reject(k);
+            });
+    };
 
 
-
-
-  return (
-    <div>
-      <FlexBoxUpDown x="space-between" upDown="10" leftRight="15">
-        <SideBar/>
-     
-
+    return (
         <div>
-          <StyledButtonLink text="Maak nieuwe kamer" to2={"/kamer/new"} icon={"fa-plus"}/>
+            <FlexBoxUpDown x="space-between" upDown="10" leftRight="15">
+                <SideBar/>
 
-          {/* <StyledRouterLink
+
+                <div>
+                    <StyledButtonLink text="Maak nieuwe kamer" to2={"/kamer/new"} icon={"fa-plus"}/>
+
+                    {/* <StyledRouterLink
             className="btn btn-pink"
             role="button"
             to={`/kamer/new`}
@@ -82,48 +103,49 @@ function Kamers(props) {
           >
             Maak nieuwe kamer
           </StyledRouterLink> */}
+                </div>
+            </FlexBoxUpDown>
+            {kamers.length<=0?<div>
+                No kamers found
+            </div>:  <GridImages width="90vw" display={"grid"} gridSize="250px">
+                {kamers.map((kamer, key) => {
+                    return (
+                        <KamerCard
+                            kamer={kamer}
+                            setKamers={setKamers}
+                            deleteKamer={deleteKamerOnClick}
+                            image={kamer.attachment}
+                            key={key}
+                        />
+
+                    );
+                })}
+            </GridImages>}
+
+            <div>
+                <button type="button">
+                    first
+                </button>
+                <button>
+                    Prev
+                </button>
+                <button disabled={true}>
+                    {pageKamerInfo.currentPage}
+                </button>
+                <button>
+                    Next
+                </button>
+
+                <button>
+                    last
+                </button>
+
+            </div>
+            <div>
+                Showing Page {pageKamerInfo.currentPage} of {pageKamerInfo.kamersPerPage}
+            </div>
         </div>
-      </FlexBoxUpDown>
-
-      <GridImages width="90vw" display={"grid"} gridSize="250px">
-        {kamers.map((kamer, key) => {
-          return (
-              <KamerCard
-                kamer={kamer}
-                setKamers={setKamers}
-                deleteKamer={deleteKamerOnClick}
-                image={kamer.attachment}
-                key={key}
-              />
-         
-          );
-        })}
-        {/* {() => getImageFromDb()} */}
-      </GridImages>
-      <div>
-        <button type="button"> 
-          first
-        </button>
-        <button>
-          Prev
-        </button>
-        <button disabled={true}>
-        {pageKamerInfo.currentPage}
-        </button>
-        <button>
-          Next
-        </button>
-
-        <button>
-          last
-        </button>
-        
-      </div>
-      <div>
-        Showing Page {pageKamerInfo.currentPage} of {pageKamerInfo.kamersPerPage}
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Kamers;
