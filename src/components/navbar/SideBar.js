5d8579df6ec3e964c09fe08fc2e51d42e2ad7c42
nathan ganesh <489290@student.saxion.nl>
2022-11-26 14:59:58 +0100

    initial start

--- src/components/navbar/SideBar.js
+++ src/components/navbar/SideBar.js
@@ -4,7 +4,6 @@ const SideBar = () => {
     return (
         <div>
               <InputWithImage placeHolderName="Naam, locatie, sterren" icon="fa-search" />
-                
         </div>
     )
 }
